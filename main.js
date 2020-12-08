54dd685ac4c7cde677b12e1d881a493468c45eaf
j3ffh95 <j3ffh95@gmail.com>
2020-12-08 13:45:42 -0500

    Finished styling section 1

---
+++ main.js
@@ -0,0 +1,9 @@
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() >= 50) {
+//     $("body").css("background", "red");
+//   } else {
+//     $("body").css("background", "transparent");
+//   }
+// });
+
+// console.log(window.scroll());
