f8e4b1d2568d8a6e63589f2ddbc587a56f09ff63
j3ffh95 <j3ffh95@gmail.com>
2020-12-14 13:07:02 -0500

    Added a navbar when scrolled

--- main.js
+++ main.js
@@ -6,4 +6,31 @@
 //   }
 // });
 
-// console.log(window.scroll());
+let showcaseSection = document.querySelector(".showcase"),
+  navElement = document.querySelector(".main-nav");
+
+let scrolled = false;
+const options = {
+  rootMargin: "-300px 0px 0px 0px",
+};
+
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      navElement.classList.remove("nav-scrolled");
+      scrolled = false;
+    } else {
+      navElement.classList.add("nav-scrolled");
+      if (!scrolled) {
+        navElement.style.transform = "translateY(-70px)";
+      }
+      setTimeout(() => {
+        navElement.style.transform = "translateY(0)";
+        scrolled = true;
+      }, 200);
+    }
+  });
+}, options);
+
+observer.observe(showcaseSection);
+// console.log(showcaseSection);
